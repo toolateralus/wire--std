e14c129ab13a86a210776702fb211be1e213e9fe
Your Name <joshnjoshgaming@gmail.com>
2024-03-29 12:46:12 -0400

    Refactor installation script and remove unused libraries

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "installing *wire* standard library... will need root access."
 sudo echo "got root access."
-mkdir -p /usr/local/include/wire
 
-sudo cp -f *.w /usr/local/include/wire
+mkdir -p /usr/local/include/wire
+sudo cp -f src/*.w /usr/local/include/wire
