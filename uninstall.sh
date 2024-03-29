0ea98ac03bd664a26e38b2c66530cdb52412b2d6
Your Name <joshnjoshgaming@gmail.com>
2024-03-29 13:23:38 -0400

    Update WIRE_IMPORT_PATH in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,10 @@
 echo "installing *wire* standard library... will need root access."
 
 if [ -z "$WIRE_IMPORT_PATH" ]; then
-  echo "WIRE_IMPORT_PATH not set, defaulting to /usr/local/include/wire"
-  WIRE_IMPORT_PATH="/usr/local/include/wire"
+  echo "WIRE_IMPORT_PATH not set, defaulting to /usr/include/wire"
+  WIRE_IMPORT_PATH="/usr/include/wire"
 fi
 
 sudo rm -rf $WIRE_IMPORT_PATH
 
-for file in src/*.w
-do
-  echo "uninstalled $file from $WIRE_IMPORT_PATH"/$(basename $file)
-done
+echo "uninstalled *wire* standard library from $WIRE_IMPORT_PATH"
